afb403a678ff6736e23f7a14e0d8cf96715d3469
Erik Peng(w3310) <wheelo@163.com>
2017-04-11 00:38:51 +0800

    Update timestamp.js

--- build/timestamp.js
+++ build/timestamp.js
@@ -1,14 +1,24 @@
-// 替换base64与添加时间戳处理
+/**
+ * @file 替换base64与添加时间戳处理
+ */
 
 // node build/timestamp.js
 var replace = require('replace');
 
-var time = Date.now();
+var rand = function(len) {
+    var x = "0123456789POIUYTREWQLKJHGFDSAMNBVCXZpoiuytrewqlkjhgfdsamnbvcxz";
+    var tmp = [];
+    for (var i = 0; i < len; i++) {
+        tmp.push(x.charAt(Math.ceil(Math.random() * 100000000) % x.length));
+    }
+    return tmp.join('');
+};
 
 replace({
-    regex: /timestamp=(.*)"/g,
-    replacement: 'timestamp=' + time + '"',
-    paths: ['./dist/b.html', './dist/a.html'],
+    regex: /(.*\.min\.js|.*\.min\.css)\"/g,
+    replacement: '$1?t=' + rand(6) + '"',
+    include: '*.html',
+    paths: ['../dist/env1/', '../dist/env2/'],
     recursive: true,
     silent: true
 });
